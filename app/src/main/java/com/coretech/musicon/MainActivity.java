c6bb1a9d04074b783c2ae3e6a37ed791d8b307f7
Tejasskadam <67118225+Tejasskadam@users.noreply.github.com>
2021-03-28 15:59:26 +0530

    MusicPlayer(MusicOn)

---
+++ app/src/main/java/com/coretech/musicon/MainActivity.java
@@ -0,0 +1,34 @@
+package com.coretech.musicon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    MediaPlayer player;
+
+    public  void play(View view){
+        player.start();
+    }
+    public void pause(View view){
+        player.pause();
+    }
+    public void stop(View view){
+        player.stop();
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        player = MediaPlayer.create(this, R.raw.alan);
+    }
+}
+
